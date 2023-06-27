c35e65761cec3de80a9b623ad3d47e0b7a9c4f0c
mateoU1297 <castro.mateo@correounivalle.edu.co>
2023-06-27 17:18:14 -0500

    se agregan modelos, servicios y controladores para items

---
+++ src/main/java/com/springboot/app/msitems/models/service/ItemService.java
@@ -0,0 +1,11 @@
+package com.springboot.app.msitems.models.service;
+
+import java.util.List;
+
+import com.springboot.app.msitems.models.Item;
+
+public interface ItemService {
+
+	public List<Item> findAll();
+	public Item findById(Long id, Integer cantidad);
+}
