b1bb383959df02ae4865f674bc11522631545925
mateoU1297 <castro.mateo@correounivalle.edu.co>
2023-06-29 18:13:09 -0500

    se implementa circuit breaker

--- src/main/java/com/springboot/app/msitems/controllers/ItemController.java
+++ src/main/java/com/springboot/app/msitems/controllers/ItemController.java
@@ -4,28 +4,54 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.app.msitems.models.Item;
+import com.springboot.app.msitems.models.Producto;
 import com.springboot.app.msitems.models.service.ItemService;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 @RestController
 public class ItemController {
-	
+
+	@Autowired
+	private CircuitBreakerFactory cbFactory;
+
 	@Autowired
 	@Qualifier("serviceFeign")
 	private ItemService itemService;
-	
+
 	@GetMapping("/listar")
-	public List<Item> listar(){
+	public List<Item> listar() {
 		return itemService.findAll();
 	}
-	
+
 	@GetMapping("/ver/{id}/cantidad/{cantidad}")
 	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
+		return cbFactory.create("items")
+				.run(() -> itemService.findById(id, cantidad), e -> metodoAlternativo(id, cantidad));
+	}
+	
+	@CircuitBreaker(name = "items", fallbackMethod = "metodoAlternativo")
+	@GetMapping("/ver2/{id}/cantidad/{cantidad}")
+	public Item detalle2(@PathVariable Long id, @PathVariable Integer cantidad) {
 		return itemService.findById(id, cantidad);
+	}
+
+	public Item metodoAlternativo(Long id, Integer cantidad) {
+		Item item = new Item();
+		Producto producto = new Producto();
+
+		item.setCantidad(cantidad);
+		producto.setId(id);
+		producto.setNombre("Camara Sony");
+		producto.setPrecio(500.00);
+		item.setProducto(producto);
+		return item;
 	}
 
 }
